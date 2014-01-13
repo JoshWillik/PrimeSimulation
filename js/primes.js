1a188a0b9f19d487defe09fee532c6dd57048a6b
Josh <joshwillik@gmail.com>
2014-01-12 21:56:28 -0500

    get prime graphing working, fix prime generator

--- js/primes.js
+++ js/primes.js
@@ -1,33 +1,55 @@
 function Grapher(container){
     this.container = document.getElementById(container);
-    this.canvasArray = {};
+    this.ctx;
+    this.pixelData;
+    this.finder = new PrimeFinder();
     
 
     this.init = function(){
-        this.addCanvas("base");
-        this.draw();
+        this.makeCanvas("base");
+        this.pixelData = this.ctx.getImageData(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
+       for(var i = 0; i < 20; i++){ 
+           var prime = this.finder.findNextPrime();
+           this.drawPixelPrime(prime);
+           console.log(prime);
+       }
     };
-    this.addCanvas = function(name){
+    this.makeCanvas = function(name){
         var canvas = document.createElement("canvas");
         this.container.appendChild(canvas);
-        this.canvasArray[name] = canvas;
+        this.ctx = canvas.getContext("2d");
     };
-    this.draw = function(){
-        var ctx = this.canvasArray.base.getContext("2d");
-        ctx.fillStyle = "red";
-        ctx.fillRect(0,0,100,100);
-    };
+    this.drawPixelPrime = function(num){
+        this.pixelData.data[(num*4)] = 255;
+        this.pixelData.data[(num*4) + 3] = 255;
+        this.ctx.putImageData(this.pixelData, 0, 0);
+    }
 }
 
 function PrimeFinder(){
-    this.lastPrime = 1;
+    this.lastPrime = 2;
+    this.matchedPrimes = [this.lastPrime];
     
     this.findNextPrime = function(){
-        var last = this.lastPrime;
+        console.log(this.matchedPrimes);
+        var prime = this.lastPrime;
         var nextPrime = 0;
         while(true){
-            
+           prime++; 
+           if(this.isPrime(prime)){
+               nextPrime = prime;
+               this.lastPrime = prime;
+               return prime;
+           }
         }
-        this.lastPrime = nextPrime; 
+    }
+    this.isPrime = function(num){
+        for(var i in this.matchedPrimes){
+            if(num % this.matchedPrimes[i] === 0){
+                return false
+            }
+        }
+        this.matchedPrimes.push(num);
+        return true;
     }
 }
